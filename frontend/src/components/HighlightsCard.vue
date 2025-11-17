a4ccbf28252c2d6a62fef695891c2b8251c5c81f
Zeredas <wondighizt2@gmail.com>
2025-11-17 09:51:40 +0800

    Action: Added HighlightsCard, & edited announcement view.

---
+++ frontend/src/components/HighlightsCard.vue
@@ -0,0 +1,38 @@
+<script setup> 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { defineProps } from "vue";
+import { 
+ArrowRight
+} from "lucide-vue-next"
+import Button from "./ui/button/Button.vue";
+
+</script>
+
+
+<template>
+    <Card class="m-0">
+    <CardHeader>
+        <CardTitle class="text-md">
+            One way traffic along Imelda Ave. — Avenida Veteranos St.
+        </CardTitle>
+        <CardDescription class="mt-2">
+            Due to the motorcade to honor the fiesta of Tacloban City, Traffic from Imelda Ave. will be redirected in a one way commodity through Avenida Veteranos. Please be guided accordingly.
+        </CardDescription>
+    </CardHeader>
+    <CardContent class="-mt-1">
+        <div class="flex items-center justify-between w-full mb-2">
+            <span class="bg-stone-950/50 px-2 py-1 text-xl font-bold rounded-lg text-[#FFF]">ROAD ADVISORY</span>
+            <p class="text-gray-400 text-xs font-regular">2 hours ago</p>
+
+        </div>
+    </CardContent>
+            
+    </Card>
+</template>
--- frontend/src/views/User/AnnouncementView.vue
+++ frontend/src/views/User/AnnouncementView.vue
@@ -1,7 +1,56 @@
 <script setup>
+import { useAuthStore } from "@/stores/auth";
+import { storeToRefs } from "pinia";
+import { onMounted } from "vue";
+import DashboardCard from "@/components/user/DashboardCard.vue";
+import AnnouncementCard from "@/components/AnnouncementCard.vue";
+import EventsCard from "@/components/EventsCard.vue";
+import HighlightsCard from "@/components/HighlightsCard.vue"
+
+
+const { logout } = useAuthStore();
+const { user } = useAuthStore();
+
 </script>
-
-
 <template>
-    
+<main>
+    <div class="w-full mx-auto">
+      <div class="w-full h-52 overflow-hidden rounded-md relative">
+        <img
+          src="@/assets/images/user-bg.jpg"
+          class="w-full h-full object-cover"
+          alt=""
+        >
+        <div class="w-full h-full bg-gradient-to-l  via-green to-primary/70 absolute top-0 left-0 z-2 flex flex-col justify-center px-8 text-white">
+            <h1 class="text-5xl font-bold">ANNOUNCEMENTS</h1>
+            <p class="font-semibold">Recent updates & announcements from the LGU</p>
+        </div>
+      </div>
+
+      <div class="mt-10">
+        <h1 class="text-2xl">Highlights</h1>
+        <div class="grid grid-cols-1 sm:grid-cols-3 space-x-4 space-y-4 gap-4 mt-4">
+            <HighlightsCard/>
+            <HighlightsCard/>
+            <HighlightsCard/>
+            <HighlightsCard/>
+            <HighlightsCard/>
+            <HighlightsCard/>
+        </div>
+      </div>
+
+      <div class="mt-10">
+        <h1 class="text-2xl">Upcoming Events</h1>
+        <div class="grid  grid-cols-1  md:grid-cols-3   lg:grid-cols-4 space-x-4 space-y-4 gap-4 mt-4">
+          <EventsCard/>
+          <EventsCard/>
+          <EventsCard/>
+          <EventsCard/>
+
+        </div>
+      </div>
+    </div>
+   
+
+  </main>
 </template>
