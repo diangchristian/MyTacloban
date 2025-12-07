21f6d874eff0138d4cf29fbe1dc494b4b0c18f38
Zeredas <wondighizt2@gmail.com>
2025-12-08 02:03:13 +0800

    Action: Added read more for user, edit & delete for admin

--- frontend/src/components/cards/AnnouncementCard.vue
+++ frontend/src/components/cards/AnnouncementCard.vue
@@ -1,42 +1,48 @@
-<script setup> 
+<script setup>
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 import { defineProps } from "vue";
-import { 
-ArrowRight
-} from "lucide-vue-next"
 import Button from "../ui/button/Button.vue";
 
-
-
+const props = defineProps({
+    role: {
+        type: String,
+        default: "user",
+    },
+});
 </script>
 
 
 <template>
     <Card class="m-0">
-    <CardHeader>
-        <CardTitle class="text-md">
-            <div class="flex items-center justify-between w-full mb-2">
-                <span class="bg-red-500/15 px-2 py-1 text-xs rounded-lg text-destructive">Urgent</span>
-                <p class="text-gray-400 text-xs font-regular">2 hours ago</p>
+        <CardHeader>
+            <CardTitle class="text-md">
+                <div class="flex items-center justify-between w-full mb-2">
+                    <span class="bg-red-500/15 px-2 py-1 text-xs rounded-lg text-destructive">Urgent</span>
+                    <p class="text-gray-400 text-xs font-regular">2 hours ago</p>
+                </div>
+                Scheduled Water Interruption - District 1
+            </CardTitle>
+            <CardDescription class="mt-2">
+                Water service will be temporarily interrupted on November 20, 2025, from 8:00 AM to 5:00 PM in District 1 for maintenance work.
+            </CardDescription>
+        </CardHeader>
+        <CardContent class="-mt-1">
 
-            </div>
-            Scheduled Water Interruption - District 1
-        </CardTitle>
-        <CardDescription class="mt-2">
-            Water service will be temporarily interrupted on November 20, 2025, from 8:00 AM to 5:00 PM in District 1 for maintenance work.
-        </CardDescription>
-    </CardHeader>
-    <CardContent class="-mt-1">
-       <Button variant="outline" class="cursor-pointer" >Read More</Button>
-        
-    </CardContent>
-            
+            <!-- add kit emit here per button depending ha action -->
+            <template v-if="role === 'admin'">
+                <Button variant="outline" class="mr-2 cursor-pointer">Edit</Button>
+                <Button variant="destructive" class="cursor-pointer">Delete</Button>
+            </template>
+            <template v-else>
+                <Button variant="outline" class="cursor-pointer">Read More</Button>
+            </template>
+        </CardContent>
     </Card>
 </template>
