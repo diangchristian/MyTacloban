d8452440dd86f373cda129d9a1078571597aa460
diangchristian <christian.diang3@gmail.com>
2025-12-07 16:16:07 +0800

    Render the data from the api routes to profile view

---
+++ frontend/src/components/ConfirmDeleteDialog.vue
@@ -0,0 +1,45 @@
+<script setup>
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
+import { useAuthStore } from '@/stores/auth';
+import { ref } from 'vue';
+
+
+const authStore =  useAuthStore()
+
+const handleClick = () =>{
+    authStore.deleteAccount()
+}
+
+</script>
+
+
+
+
+<template>
+    <AlertDialog v-model:open="isDialogOpen">
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction class="bg-destructive" @click="handleClick"> Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  </template>
