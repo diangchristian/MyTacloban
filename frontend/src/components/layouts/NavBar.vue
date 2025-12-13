4b81fdc809727346a103c09f5aa053f1991d9f7d
diangchristian <christian.diang3@gmail.com>
2025-12-13 15:17:42 +0800

    fixed auth when registering

--- frontend/src/components/layouts/NavBar.vue
+++ frontend/src/components/layouts/NavBar.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-
+import Button from '../ui/button/Button.vue';
 
 </script>
 
@@ -14,8 +14,15 @@ import { RouterLink } from 'vue-router';
       </span>
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MyTacloban</span>
   </a>
-  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-     <RouterLink :to="{name: 'register'}" class="hidden sm:block text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Account</RouterLink>
+  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse gap-2">
+  <div class="gap-2 hidden space-x-2 sm:block">
+    <Button asChild >
+        <RouterLink :to="{name: 'login'}" >Login</RouterLink>
+      </Button>
+      <Button asChild variant="outline">
+        <RouterLink :to="{name: 'register'}" >Sign Up</RouterLink>
+      </Button>
+  </div>
       <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -28,7 +35,9 @@ import { RouterLink } from 'vue-router';
     <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <RouterLink  :to="{name: 'home'}" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500">Home</RouterLink>
       <RouterLink  :to="{name: 'home'}" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</RouterLink>
-      <RouterLink :to="{name: 'register'}" class="block sm:hidden text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Account</RouterLink>
+      <Button asChild>
+        <RouterLink :to="{name: ''}" class="block sm:hidden text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Account</RouterLink>
+      </Button>
     </ul>
   </div>
   </div>
